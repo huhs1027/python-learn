c7a32a3ad9edfa2475868d6e81b682d485acb757
huhongsen <huhongsen@quannengzhanggui.cn>
2019-04-13 16:18:23 +0800

    fzdm下载完善 多线程实现

--- common/__init__.py
+++ common/__init__.py
@@ -1,12 +1,14 @@
 import os
 
+import requests
 
-def down(fileDir, fileName, content):
+
+def down(fileDir, fileName, url, headers):
     if not os.path.exists(fileDir):
         os.makedirs(fileDir)
     if os.path.exists(fileName):
-        print(fileName + "已存在")
+        #print(fileName + "已存在")
         return
     file = open(fileName, 'wb')
-    file.write(content)
+    file.write(requests.get(url, headers=headers).content)
     file.close()
--- fzdm/huoying.py
+++ fzdm/huoying.py
@@ -1,11 +1,41 @@
+import threading
+import time
+
 import requests
 from bs4 import BeautifulSoup
 
 import common
 import fzdm
 
+
+def batch_down(tag):
+    ye = 0
+    title = ""
+    while True:
+        # 下载
+        htmlUrl = url + tag.find('a')['href'] + "/index_{}.html".format(str(ye))
+        # print(htmlUrl)
+        resp = requests.get(htmlUrl, headers=heads, verify=False)
+        if resp.status_code == 200:
+            html = resp.text
+            # bs = BeautifulSoup(html, 'lxml')
+            # print(bs.prettify())
+
+            tupiandizhi, title, next_url = fzdm.prase(html)
+            print(tupiandizhi)
+
+            # 下载
+            dir = baseDir + '\\' + title
+            file = dir + "\\" + str(ye + 1) + ".jpg"
+            common.down(dir, file, tupiandizhi, heads)
+            ye += 1
+        else:
+            print(title + "下载完成")
+            break
+
+
 if __name__ == '__main__':
-    #取消https ssl验证警告
+    # 取消https ssl验证警告
     requests.packages.urllib3.disable_warnings()
     baseDir = "C:\huhongsen\pythonspace\picture\火影忍者"
     # 目录页
@@ -23,25 +53,6 @@ if __name__ == '__main__':
 
     # 遍历话数
     for tag in list:
-        ye = 0
-        while True:
-            # 下载
-            htmlUrl = url + tag.find('a')['href'] + "/index_{}.html".format(str(ye))
-            #print(htmlUrl)
-            resp = requests.get(htmlUrl, headers=heads, verify=False)
-            if resp.status_code == 200:
-                html = resp.text
-                bs = BeautifulSoup(html, 'lxml')
-                # print(bs.prettify())
-
-                tupiandizhi, title, next_url = fzdm.prase(html)
-                print(tupiandizhi)
-
-                # 下载
-                dir = baseDir + '\\' + title
-                file = dir + "\\" + str(ye + 1) + ".jpg"
-                common.down(dir, file, requests.get(tupiandizhi, headers=heads).content)
-                ye += 1
-            else:
-                print("下一话")
-                break
+        threading.Thread(target=batch_down, args=(tag,)).start()
+        time.sleep(10)
+        # batch_down(tag)
